07e35b13c847a1063d88f836c93f049bcc441ec6
SoumyadeepOSD <opencontainer255@gmail.com>
2022-12-18 19:07:10 +0530

    Files Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { StreamChat } from "stream-chat";
+import {
+  Chat,
+  Channel,
+  Window,
+  ChannelHeader,
+  MessageList,
+  MessageInput,
+  Thread,
+  LoadingIndicator,
+  ChannelList,
+} from "stream-chat-react";
 
-function App() {
+import "stream-chat-react/dist/css/index.css";
+
+const apiKey = process.env.REACT_APP_API_KEY;
+
+
+  const user = 
+  {
+    id: "john",
+    name: "John",
+    image: "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/rqmbxuwmevgboyuazyct",
+  }
+
+
+const filters = { type: "messaging", members: { $in: [user.id] } };
+const sort = { last_message_at: -1 };
+
+function CustomChannelPreview(props) {
+  const { channel, SetActiveChannel } = props;
+  const { messages } = channel.state;
+  const lastMessage = messages[messages.length - 1];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button
+      onClick={() => SetActiveChannel(channel)}
+      style={{ margin: "12px" }}
+    >
+      <div>{channel.data.name || "Unnamed Channel"}</div>
+      <div style={{ fontSize: "20px" }}>{lastMessage}</div>
+    </button>
   );
 }
 
-export default App;
+export default function Project() {
+  const [client, setClient] = useState(null);
+
+  useEffect(() => {
+    async function init() {
+      const chatClient = StreamChat.getInstance(apiKey);
+
+      await chatClient.connectUser(user, chatClient.devToken(user.id));
+
+      const channel = chatClient.channel("messaging", "community-talk", 
+      {
+        image: "https://th.bing.com/th/id/OIP.eDvXzty9BCWSgcOPZZMB8gHaHa?pid=ImgDet&rs=1",
+        name: "DevOps Roadmap",
+        members: [user.id],
+      });
+
+      await channel.watch();
+
+      setClient(chatClient);
+    }
+    init();
+    if (client) return () => client.disconnectUser();
+  }, []);
+
+  if (!client) return <LoadingIndicator/>;
+
+  return (
+    <Chat client={client} theme="messaging dark">
+      <ChannelList
+        filters={filters}
+        sort={sort}
+      />
+        <Channel>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+}
